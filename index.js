24f1406b48b0ccb8488b69364163878ddfa5e802
Mykola-Veres <veresnickolay@gmail.com>
2022-05-18 13:23:10 +0300

    add-index

---
+++ index.js
@@ -0,0 +1,15 @@
+const data = new Date();
+
+console.log(data.getFullYear())
+
+const date1 = new Date('December 17, 1995 03:24:00');
+// Sun Dec 17 1995 03:24:00 GMT...
+
+const date2 = new Date('1995-12-17T03:24:00');
+// Sun Dec 17 1995 03:24:00 GMT...
+
+console.log(date1 === date2);
+// expected output: false;
+
+console.log(date1 - date2);
+// expected output: 0
